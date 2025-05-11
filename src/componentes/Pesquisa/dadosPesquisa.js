c29e79ec6e0d874066d3e8ae4401a384e74615be
nsfge <nsfgeo@hotmail.com>
2025-05-10 23:05:36 -0300

    "criando controle de estado"

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from "../../imagens/livro.png";
+import livro2 from "../../imagens/livro2.png";
+
+export const livros = [
+  { nome: "Liderança em Design", id: 1, src: livro },
+  { nome: "JavaScript Assertivo", id: 2, src: livro2 },
+  { nome: "Cangaceiro em JavaScript", id: 3, src: livro },
+  { nome: "Aventureiros em Java", id: 4, src: livro2 },
+  { nome: "Liderança em UX", id: 5, src: livro },
+];
